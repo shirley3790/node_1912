0b1c43fa6bb0cbfb1f227b36847e6a1deb45203d
malin <348475822@qq.com>
2019-12-24 16:08:28 +0800

    new

--- 02express/04express_server.js
+++ 02express/04express_server.js
@@ -11,7 +11,7 @@ const app = express();
 //借助中间件开启静态资源服务器
 
 app.use(express.static('./'));//以当前目录为站点根目录
-app.use(allRouter);
+app.use(allRouter);//使用allRouter中间件，实现路由效果
 
 app.listen(1912, () => {
     console.log('服务器成功开启,请访问1912端口');
