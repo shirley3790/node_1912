5d2d700982b69f8a0bfb4e58174138905cbefe06
malin <348475822@qq.com>
2019-12-25 15:45:31 +0800

    day3

--- 03数据库/myapp/backend/routers/users.js
+++ 03数据库/myapp/backend/routers/users.js
@@ -9,19 +9,18 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 /*
     用户管理：
-        * 验证用户名是否存在
-        * 注册
+        * 验证用户名是否存在 完成
+        * 注册 完成
         * 登陆
         * 修改密码
         * 删除xx用户
         * 清空用户
-        * 数据渲染
-        * 分页
+        * 数据渲染 分页 完成
 */
 
 //需求：查询所有的用户信息
-Router.get('/all', (req, res) => {
-    let str = 'SELECT * FROM userinf';
+Router.get('/all', async (req, res) => {
+    // let str = 'SELECT * FROM userinf';
     //初级版
     // query(str, data => {
     //     //形参
@@ -29,49 +28,118 @@ Router.get('/all', (req, res) => {
     //     res.send(data);//把查询到的数据响应给前端
     // });
 
+    //中级版
+    // let p = query(str);
+    // p.then(data => {
+
+    // });
+
     //高级版
-    
+    //前端传：第几页page 传数量 num
+    let { page, num } = req.query;
+    // console.log(page, num);
+    page = page || 1;//设置默认值
+    num = num || 5;//设置默认值
+    let index = (page - 1) * num;
+    let str = `SELECT * FROM userinf LIMIT ${index},${num}`;
+    let data = await query(str);
+    // console.log(data);
+
+    let sql2 = 'SELECT * FROM userinf';
+    let data2 = await query(sql2);
+    /*
+        result :{
+            type : 1,
+            msg : 'success',
+            page : 1,
+            num : 5,
+            pages : 6,
+            datalist : []
+        }
+    */
+
+    let result = {};
+    if (data.length) {
+        //成功返回的数据
+        let pages = Math.ceil(data2.length / num);
+        result = {
+            type: 1,
+            msg: 'success',
+            page,
+            num,
+            pages,
+            datalist: data
+        }
+    } else {
+        //失败返回的数据
+        result = {
+            type: 0,
+            msg: 'fail',
+            datalist: []
+        }
+    }
+
+    res.send(result);
 });
 
 //需求：验证用户名是否存在
 // let userlist = ['admin', 'lubao', 'xiaochong', 'xiaohu'];
 //  /users/checkname
-Router.get('/checkname', (req, res) => {
+Router.get('/checkname', async (req, res) => {
     //接收前端数据
     // console.log(req);
     let { name } = req.query;
-    console.log(name);//做数据库的查询
-    let result = userlist.some(item => item == name);
-
-    let data = {};
-    if (result) {//真：找到，不给注册
-        data = {
-            type: 0,
-            msg: '失败'
+    // console.log(name);//做数据库的查询
+    if (name) {
+        let sql = `SELECT * FROM userinf WHERE name = '${name}'`;
+        let data = await query(sql);
+        let result = {};
+        if (data.length) { // '0' 真  0 假
+            //查找到了，不给注册
+            result = {
+                type: 0,
+                msg: '不能注册'
+            }
+        } else {
+            //可以注册
+            result = {
+                type: 1,
+                msg: '可以注册'
+            }
         }
-    } else {
-        data = {
-            type: 1,
-            msg: '成功'
-        }
+        res.send(result);//无论失败与成功都需要响应给客户端
     }
-    res.send(data);//无论失败与成功都需要响应给客户端
+    res.send('数据不能为空');
 });
 
 //需求：注册功能
 
-// Router.post('/reg', express.urlencoded(), (req, res) => {
-//     let obj = req.body;
-//     console.log(obj);
-//     res.send('成');
-// });
-
-Router.post('/reg', urlencodedParser, (req, res) => {
+Router.post('/reg', urlencodedParser, async (req, res) => {
     // let obj = req.body;
-    let { name: username, password } = req.body;//解构
-    // console.log(obj);
-    console.log(username, password);//把用户名和密码插入数据库
-    res.send('成');
+    let { name, password } = req.body;//解构
+    if (name && password) {
+        let sql = `INSERT INTO userinf(name,psw) VALUES('${name}','${password}')`;
+        let data = await query(sql);
+        // console.log(data);
+        let result = {};
+        if (data.affectedRows) {
+            //插入成功
+            result = {
+                type: 1,
+                msg: '注册成功'
+            }
+        } else {
+            //插入失败
+            result = {
+                type: 0,
+                msg: '注册失败'
+            }
+        }
+        res.send(result);
+    } else {
+        res.send('请填写全部数据');
+    }
+
 });
 
 module.exports = Router;
--- 03数据库/myapp/server.js
+++ 03数据库/myapp/server.js
@@ -11,6 +11,7 @@ const app = express();
 //借助中间件开启静态资源服务器
 
 app.use(express.static('./'));//以当前目录为站点根目录
+// app.use(express.static('./', { index: './adminsystem/admin.html' }));//以当前目录为站点根目录
 app.use(allRouter);//使用allRouter中间件，实现路由效果
 
 app.listen(1912, () => {
