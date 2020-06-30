82ce4a9988e04e13a4af6f806536fb1fe55f928f
shirley <348475822@qq.com>
2020-06-30 15:33:22 +0800

    更新

--- 03mysql/myapp/backend/routers/index.js
+++ 03mysql/myapp/backend/routers/index.js
@@ -12,6 +12,21 @@ const Router = express.Router();//路由设置  Router==app
         * 订单管理 /orders
 */
 
+//CORS跨域
+//把这个路由配置放在所有路由的前面，方便调用next操作
+Router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,PATCH,POST,GET,DELETE,OPTIONS");
+
+    // 跨域请求CORS中的预请求
+    if (req.method == "OPTIONS") { //特殊请求：发送了请求头的那些请求
+        res.sendStatus(200); /*让options请求快速返回*/
+    } else {
+        next();
+    }
+})
+
 Router.use('/users', usersRouter);//mysql做数据库
 Router.use('/goods', goodsRouter);//mongodb做数据库
 
